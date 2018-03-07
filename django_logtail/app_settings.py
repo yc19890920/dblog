d1c12473b34e203174640bb8d848e660c4eb9d07
yangcheng <1248644045@qq.com>
2018-03-07 11:52:03 +0800

    django logtail集成

--- django_logtail/app_settings.py
+++ django_logtail/app_settings.py
@@ -10,6 +10,10 @@ LOGTAIL_UPDATE_INTERVAL = getattr(settings, 'LOGTAIL_UPDATE_INTERVAL', '3000')
 LOGTAIL_FILES = {
     'celerybeat': "/usr/local/log/dblog/dblog-celery-beat.log",
     'celeryworker': "/usr/local/log/dblog/dblog-celery-worker.log",
+    'apache': "/usr/local/log/dblog/dblog_apache.log",
+    'apache_error': "/usr/local/log/dblog/dblog_apache_error.log",
+    'nginx': "/usr/local/log/dblog/dblog_nginx.log",
+    'nginx_error': "/usr/local/log/dblog/dblog_nginx_erorr.log",
 }
 
 # for k, v in PROCESSES.iteritems():
