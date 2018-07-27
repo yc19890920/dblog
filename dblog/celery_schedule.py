b9769b83ce770ad12ec7cd13a53c048f2dbf2ff6
yangcheng <1248644045@qq.com>
2018-07-27 14:18:45 +0800

    流量监控

--- dblog/celery_schedule.py
+++ dblog/celery_schedule.py
@@ -8,14 +8,14 @@ CELERYBEAT_SCHEDULE = {
     # TCP连接情况
     'tcp_connect_info': {
         'task': 'app.core.tasks.tcp_connect_info',
-        'schedule': crontab(minute="*/1"),
+        'schedule': crontab(minute="*/2"),
         'args': ()
     },
 
     # 网卡流量监控
     'network_monitor_info': {
         'task': 'app.core.tasks.network_monitor_info',
-        'schedule': crontab(minute="*/1"),
+        'schedule': crontab(minute="*/2"),
         'args': ()
     },
 }
