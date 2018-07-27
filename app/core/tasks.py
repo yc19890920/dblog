39e1fbd541eca405bedb75df16733ff2f8fceac1
yangcheng <1248644045@qq.com>
2018-07-27 10:58:59 +0800

    流量监控

--- app/core/tasks.py
+++ app/core/tasks.py
@@ -11,14 +11,14 @@ import datetime
 import netifaces
 from django_redis import get_redis_connection
 from collections import defaultdict, OrderedDict
-from app.utils import network
+from app.utils.sys import network
 from django.utils.translation import ugettext_lazy as _
 
-DJANGO_CELERY_TCP_CONNECT_INFO = "dblog-celery-tcp-connect-info-{time}"
-DJANGO_CELERY_TCP_CONNECT_INFO_SHOW = "dblog-celery-tcp-connect-info-show-{time}"
+DJANGO_CELERY_TCP_CONNECT_INFO = "linux-operation-celery-tcp-connect-info-{time}"
+DJANGO_CELERY_TCP_CONNECT_INFO_SHOW = "linux-operation-celery-tcp-connect-info-show-{time}"
 
-DJANGO_CELERY_NET_MONITOR_INFO = "dblog-net-monitor-info-{network}-{time}"
-DJANGO_CELERY_NET_MONITOR_INFO_SHOW = "dblog-net-monitor-info-show-{network}"
+DJANGO_CELERY_NET_MONITOR_INFO = "linux-operation-celery-net-monitor-info-{network}-{time}"
+DJANGO_CELERY_NET_MONITOR_INFO_SHOW = "linux-operation-celery-net-monitor-info-show-{network}-{time}"
 
 KB = 1 << 10
 MB = 1 << 20
@@ -204,7 +204,7 @@ def _get_tcp_connect_info(redis, key, bn="now"):
                 "compactdatamode" : "1",
                 "dataseparator" : "|",
                 "labelHeight": "30",
-                "scrollheight": "2",
+                "scrollheight": "10",
                 "flatScrollBars": "1",
                 "scrollShowButtons": "0",
                 "scrollColor": "#cccccc",
@@ -252,40 +252,45 @@ def network_monitor_info():
                .lrange(key, 0, -1)
                .execute())
         stat_net = res[-1]
-        set_network_monitor_info(redis, reversed(stat_net), k, bnow_fmt, bbnow_fmt)
+        set_network_monitor_info(redis, reversed(stat_net), k, now_fmt, bnow_fmt, bbnow_fmt)
 
-def set_network_monitor_info(redis, nstat_net, net, bnow_fmt, bbnow_fmt):
+def set_network_monitor_info(redis, nstat_net, net, now_fmt, bnow_fmt, bbnow_fmt):
     bkey = DJANGO_CELERY_NET_MONITOR_INFO.format(network=net, time=bnow_fmt)
     bbkey = DJANGO_CELERY_NET_MONITOR_INFO.format(network=net, time=bbnow_fmt)
     bstat_net = reversed(redis.lrange(bkey, 0, -1))
     bbstat_net = reversed(redis.lrange(bbkey, 0, -1))
-    key = DJANGO_CELERY_NET_MONITOR_INFO_SHOW.format(network=net)
-    stat_net = []
-    stat_net.extend(bbstat_net)
-    stat_net.extend(bstat_net)
-    stat_net.extend(nstat_net)
-    categories = []
-    datasets = defaultdict(list)
-    for x in stat_net:
-        x = json.loads(x)
-        category = x['time']
-        categories.append(category)
-        x1, x2 = x['results']
-        datasets['Incoming network traffic on {}'.format(net)].append(x1)
-        datasets['Outgoing network traffic on {}'.format(net)].append(x2)
-    datasets = dict(datasets)
-    datasets_lists = []
-    for k, v in datasets.iteritems():
-        datasets_lists.extend(v)
-    yaxisminValue = min(datasets_lists)
-    # yaxisminValue = min([i for i in datasets_lists if i>0])
-    yaxismaxValue = max(datasets_lists)
-    redis.set(key, json.dumps({
-        "datasets": datasets,
-        "categories": categories,
-        "yaxisminValue": yaxisminValue,
-        "yaxismaxValue": yaxismaxValue,
-    }))
+    skey = DJANGO_CELERY_NET_MONITOR_INFO_SHOW.format(network=net, time=now_fmt)
+    bskey = DJANGO_CELERY_NET_MONITOR_INFO_SHOW.format(network=net, time=bnow_fmt)
+    bbskey = DJANGO_CELERY_NET_MONITOR_INFO_SHOW.format(network=net, time=bbnow_fmt)
+    # stat_net = []
+    # stat_net.extend(bbstat_net)
+    # stat_net.extend(bstat_net)
+    # stat_net.extend(nstat_net)
+    for key, stats in [(skey, nstat_net),(bskey, bstat_net),(bbskey, bbstat_net)]:
+        categories = []
+        datasets = defaultdict(list)
+        for x in stats:
+            x = json.loads(x)
+            category = x['time']
+            categories.append(category)
+            x1, x2 = x['results']
+            datasets['Incoming network traffic on {}'.format(net)].append(x1)
+            datasets['Outgoing network traffic on {}'.format(net)].append(x2)
+        datasets = dict(datasets)
+        datasets_lists = []
+        for k, v in datasets.iteritems():
+            datasets_lists.extend(v)
+        yaxisminValue = min(datasets_lists)
+        # yaxisminValue = min([i for i in datasets_lists if i>0])
+        yaxismaxValue = max(datasets_lists)
+        ( redis.pipeline()
+          .set(key, json.dumps({
+            "datasets": datasets,
+            "categories": categories,
+            "yaxisminValue": yaxisminValue,
+            "yaxismaxValue": yaxismaxValue,}))
+          .expire(key, EXPIRE_TIME)
+          .execute())
 
 def get_network_monitor_info():
     key_info = network.get_nets()  # 获取网卡名称
@@ -295,105 +300,134 @@ def get_network_monitor_info():
     routingNicName = str(netifaces.gateways()['default'][netifaces.AF_INET][1])
     key_info.remove(routingNicName)
     key_info.insert(0, routingNicName)
+    now = datetime.datetime.now()
+    bnow_fmt = (now - datetime.timedelta(days=1)).strftime("%Y-%m-%d")
+    bbnow_fmt = (now - datetime.timedelta(days=2)).strftime("%Y-%m-%d")
+    now_fmt = now.strftime("%Y-%m-%d")
     for k in key_info:
-        d = _get_network_monitor_info(redis, k)
+        d = _get_network_monitor_info(redis, k, now_fmt, bnow_fmt, bbnow_fmt)
         if d:
             keys.append(k)
             j.append( (k, d) )
     return keys, j
 
-def _get_network_monitor_info(redis, net):
-    key = DJANGO_CELERY_NET_MONITOR_INFO_SHOW.format(network=net)
-    j = redis.get(key)
-    if not j: return None
-    d = json.loads(j)
-    categories = d["categories"]
-    length = len(categories)
-    index = 0
-    while 1:
-        if index>=length:break
-        _c = categories[index]
-        if _c.endswith(":00"):
-            break
-        index +=1
-    datasets = d["datasets"]
-    yaxismaxValue = d["yaxismaxValue"]
-    _unit, _unit_str, _unit_round = KB, 'KB/s', 1
-    if yaxismaxValue>=MB:
-        _unit, _unit_str, _unit_round  = MB, 'MB/s', 2
-    yaxisminValue = 0
-    yaxismaxValue = network.trans_io(d["yaxismaxValue"], _unit, _unit_round)
-    category = "|".join(categories[index:])
-    _in_key = 'Incoming network traffic on {}'.format(net)
-    _out_key = 'Outgoing network traffic on {}'.format(net)
-    _ins = [network.trans_io(i, _unit, _unit_round) for i in datasets[_in_key][index:]]
-    _outs = [network.trans_io(i, _unit, _unit_round) for i in datasets[_out_key][index:]]
-    dataset = [{
-        "seriesname": _in_key,
-        "data": "|".join(map(str, _ins))
-    },{
-        "seriesname": _out_key,
-        "data": "|".join(map(str, _outs))
-    }]
-    caption = _(u'%(name)s网卡流量监控') % {'name': net}
-    js = {
-        'type': 'zoomline',
-        'renderAt': 'tcp-info-container-{}'.format(net),
-        'width': '100%',
-        'height': '500',
-        'dataFormat': 'json',
-        'dataSource': {
-            "chart": {
-                "caption": caption,  # 图表主标题
-                "subcaption": "",  # 图表副标题
-                "paletteColors": "#c79121,#447ebc",
-                "captionFontSize": "16",
-                "subcaptionFontSize": "16",
-                "subcaptionFontBold": "0",
-                "showBorder": "0",
-                "bgColor": "#ffffff", # 图表背景色，6位16进制颜色值
-                "canvasBgColor": "#1f1d1d",
-                # "canvasBgColor": "#ffffff",  # 画布背景色，6位16进制颜色值
-                # "bgColor": "#1f1d1d",
-                "baseFont": "Helvetica Neue,Arial", # 图表字体样式
-                "baseFontSize": "12", # 图表字体大小
-                "showCanvasBorder": "0",
-                "showShadow": "0",
-                "showAlternateHGridColor": "0",
-                "yaxisname": _(u"流量") % {}, # 纵向坐标轴(y轴)名称
-                "xaxisname": _(u"时间") % {}, # 横向坐标轴(x轴)名称
-                "yaxisminValue": "{}".format(yaxisminValue),
-                "yaxismaxValue": "{}".format(yaxismaxValue),
-                "forceAxisLimits" : "1",
-                "pixelsPerPoint": "0",
+def _get_network_monitor_info(redis, net, now_fmt, bnow_fmt, bbnow_fmt):
+    # key = DJANGO_CELERY_NET_MONITOR_INFO_SHOW.format(network=net, time=now_fmt)
+    skey = DJANGO_CELERY_NET_MONITOR_INFO_SHOW.format(network=net, time=now_fmt)
+    bskey = DJANGO_CELERY_NET_MONITOR_INFO_SHOW.format(network=net, time=bnow_fmt)
+    bbskey = DJANGO_CELERY_NET_MONITOR_INFO_SHOW.format(network=net, time=bbnow_fmt)
+    datajs = []
+    days = [('today', _(u'今日'), skey, now_fmt), ('yest', _(u'昨日'), bskey, bnow_fmt), ('bday', _(u'前日'), bbskey, bbnow_fmt)]
+    for daykey, day, key, day_fmt in days:
+        j = redis.get(key)
+        if not j:
+            datajs.append((daykey, day, None))
+            continue
+        d = json.loads(j)
+        categories = d["categories"]
+        length = len(categories)
+        index = 0
+        while 1:
+            if index>=length:break
+            _c = categories[index]
+            if _c.endswith(":00"):
+                break
+            index +=1
+        datasets = d["datasets"]
+        yaxismaxValue = d["yaxismaxValue"]
+        _unit, _unit_str, _unit_round = KB, 'KB/s', 1
+        if yaxismaxValue>= MB:
+            _unit, _unit_str, _unit_round  = MB, 'MB/s', 2
+        yaxisminValue = 0
+        yaxismaxValue = network.trans_io(d["yaxismaxValue"], _unit, _unit_round)
+        yaxismaxValue = get_yaxismaxValue(yaxismaxValue, _unit)
+        category = "|".join(categories[index:])
+        _in_key = 'Incoming network traffic on {}'.format(net)
+        _out_key = 'Outgoing network traffic on {}'.format(net)
+        _ins = [network.trans_io(i, _unit, _unit_round) for i in datasets[_in_key][index:]]
+        _outs = [network.trans_io(i, _unit, _unit_round) for i in datasets[_out_key][index:]]
+        dataset = [{
+            "seriesname": _in_key,
+            "data": "|".join(map(str, _ins))
+        },{
+            "seriesname": _out_key,
+            "data": "|".join(map(str, _outs))
+        }]
+        caption = _(u'%(name)s网卡%(day)s流量监控') % {'name': net, "day": day_fmt}
+        js = {
+            'type': 'zoomline',
+            'renderAt': 'net-info-container-{}-{}'.format(net, daykey),
+            'width': '100%',
+            'height': '500',
+            'dataFormat': 'json',
+            'dataSource': {
+                "chart": {
+                    "caption": caption,  # 图表主标题
+                    "subcaption": "",  # 图表副标题
+                    "paletteColors": "#c79121,#447ebc",
+                    "captionFontSize": "16",
+                    "subcaptionFontSize": "16",
+                    "subcaptionFontBold": "0",
+                    "showBorder": "0",
+                    "bgColor": "#ffffff", # 图表背景色，6位16进制颜色值
+                    "canvasBgColor": "#1f1d1d",
+                    # "canvasBgColor": "#ffffff",  # 画布背景色，6位16进制颜色值
+                    # "bgColor": "#1f1d1d",
+                    "baseFont": "Helvetica Neue,Arial", # 图表字体样式
+                    "baseFontSize": "12", # 图表字体大小
+                    "showCanvasBorder": "0",
+                    "showShadow": "0",
+                    "showAlternateHGridColor": "0",
+                    "yaxisname": _(u"流量") % {}, # 纵向坐标轴(y轴)名称
+                    "xaxisname": _(u"时间") % {}, # 横向坐标轴(x轴)名称
+                    "yaxisminValue": "{}".format(yaxisminValue),
+                    "yaxismaxValue": "{}".format(yaxismaxValue),
+                    "forceAxisLimits" : "1",
+                    "pixelsPerPoint": "0",
 
-                "pixelsPerLabel": "12",  #间隔，每60分钟一个间隔
+                    "pixelsPerLabel": "12",  #间隔，每60分钟一个间隔
 
-                "lineThickness": "1",
-                "compactdatamode" : "1",
-                "dataseparator" : "|",
-                "labelHeight": "30",
-                "scrollheight": "2",
-                "flatScrollBars": "1",
-                "scrollShowButtons": "0",
-                "scrollColor": "#cccccc",
-                "legendBgAlpha": "0",
-                "legendBorderAlpha": "0",
-                "legendShadow": "0",
-                "legendItemFontSize": "14", # 项目字体大小
-                "legendItemFontColor": "#666666",
-                "formatNumberScale":"0",
-                "showhovercap": True,
-                "labelStep": "{}".format(labelStep),  # 间隔，每60分钟一个间隔
-                # "numVDivLines": "12", # 垂直分割线
-                "numberSuffix": _unit_str,# 增加数字后缀
-                # "numberPrefix": "KB/S", # 增加数字前缀     % 为 '%25'
-            },
-            "categories": [{
-                "category":  "{}".format(category)
-            }],
-            "dataset": dataset
+                    "lineThickness": "1",
+                    "compactdatamode" : "1",
+                    "dataseparator" : "|",
+                    "labelHeight": "30",
+                    "scrollheight": "10", #
+                    "flatScrollBars": "1",
+                    "scrollShowButtons": "0",
+                    "scrollColor": "#cccccc",
+                    "legendBgAlpha": "0",
+                    "legendBorderAlpha": "0",
+                    "legendShadow": "0",
+                    "legendItemFontSize": "14", # 项目字体大小
+                    "legendItemFontColor": "#666666",
+                    "formatNumberScale":"0",
+                    "showhovercap": True,
+                    "labelStep": "{}".format(labelStep),  # 间隔，每60分钟一个间隔
+                    # "numVDivLines": "12", # 垂直分割线
+                    "numberSuffix": _unit_str,# 增加数字后缀
+                    # "numberPrefix": "KB/S", # 增加数字前缀     % 为 '%25'
+                    "numDivLines": 5, # 水平分割线
+                },
+                "categories": [{
+                    "category":  "{}".format(category)
+                }],
+                "dataset": dataset
+            }
         }
-    }
-    return json.dumps(js)
+        datajs.append((daykey, day, json.dumps(js)))
+    return datajs
 
+
+def get_yaxismaxValue(yaxismaxValue, unit):
+    if unit == MB:
+        d = 1.2
+        while 1:
+            if d>=yaxismaxValue:
+                return d
+            d += 0.3
+    else:
+        d = 30
+        while 1:
+            if d >= yaxismaxValue:
+                return d
+            d += 30
